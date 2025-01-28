9f6666f5d58e3f4b83309b9fd021388614a40671
Goreti18 <goreti.pizarrop@uanl.edu.mx>
2025-01-27 19:08:34 -0600

    Add files via upload

--- Ejercicios python.py
+++ Ejercicios python.py
@@ -1,45 +1,44 @@
-Ejercicio 1
-¿Cuál es la función de la memoria secundaria en una computadora?
-R= Almacenar información durante mucho tiempo, incluso después de ciclos de apagado y encendido
+print('Ejercicio 1')
+print('Cuál es la función de la memoria secundaria en una computadora?')
+print('R= Almacenar información durante mucho tiempo, incluso después de ciclos de apagado y encendido')
 
-Ejercicio 2
-Un programa son instrucciones almacenadas (órdenes) que se utilizan para la realización de alguna tarea específica.
+print('Ejercicio 2')
+print('Un programa son instrucciones almacenadas (órdenes) que se utilizan para la realización de alguna tarea específica.')
 
-Ejercicio 3
-¿Cuál es la diferencia entre un compilador y un intérprete?
-R=un intérprete lee el código del programa tal y como fue escrito por el programador, lo analiza e interpreta las instrucciones sobre la marcha, y es capaz de mantener una conversación interactiva, mientras que un compilador necesita el programa completo y luego ejecuta un proceso para traducir el código fuente de alto nivel a código máquina para luego tener un fichero con su ejecución
+print('Ejercicio 3')
+print('Cuál es la diferencia entre un compilador y un intérprete?')
+print('R=un intérprete lee el código del programa tal y como fue escrito por el programador, lo analiza e interpreta las instrucciones sobre la marcha, y es capaz de mantener una conversación interactiva, mientras que un compilador necesita el programa completo y luego ejecuta un proceso para traducir el código fuente de alto nivel a código máquina para luego tener un fichero con su ejecución')
 
-Ejercicio 4
-¿Cuál de los siguientes contiene "código maquina"?
-R= c) el código fuente de Python
+print('Ejercicio 4')
+print('Cuál de los siguientes contiene "código maquina"?')
+print('R= c) el código fuente de Python')
 
-Ejercicio 5
-¿Qué está mal en el siguiente código?
-R= "primt" está mal escrito, faltan los paréntesis en la frase y el mensaje puede ser escrito de una forma más sencilla
-como:
+print('Ejercicio 5')
+print('Qué está mal en el siguiente código?')
+print('R= "primt" está mal escrito, faltan los paréntesis en la frase y el mensaje puede ser escrito de una forma más sencilla')
+print('como:')
 print("¡Hola, mundo!")
 
+print('Ejercicio 6')
+print('En qué lugar del computador queda almacenada una variable, como en este caso "x", después de ejecutar la siguiente línea de Python?')
+print('x=123')
+print('R= Memoria principal')
 
-Ejercicio 6
-¿En qué lugar del computador queda almacenada una variable, como en este caso "x", después de ejecutar la siguiente línea de Python?
-x=123
-R= Memoria principal
-
-Ejercicio 7
-¿Qué mostrará en pantalla el siguiente programa?
+print('Ejercicio 7')
+print('Qué mostrará en pantalla el siguiente programa?')
 x = 43
 x = x + 1
 print(x)
-R= 44
+print('R= 44')
 
-Ejercicio 8
-1. Unidad central de procesamiento: el cerebro
-2. Memoria principal: memoria a corto plazo
-3. Memoria secundaria: memoria a largo plazo
-4. Dispositivos de entrada: oídos/capacidad de escuchar e incluso cualquiera de nuestros sentidos
-5. Dispositivos de salida: boca/hablar/expresarnos
+print('Ejercicio 8')
+print('1. Unidad central de procesamiento: el cerebro')
+print('2. Memoria principal: memoria a corto plazo')
+print('3. Memoria secundaria: memoria a largo plazo')
+print('4. Dispositivos de entrada: oídos/capacidad de escuchar e incluso cualquiera de nuestros sentidos')
+print('5. Dispositivos de salida: boca/hablar/expresarnos')
 
 
-Ejercicio 9
-¿Cómo puedes corregir un "Error de sintaxis"?
-Debido a que el error de sintaxis es una violación de las reglas gramaticales, python te indica dónde fue detectado el fallo y puede solucionarse encontrando el error y corrigiendo la gramática
+print('Ejercicio 9')
+print('Cómo puedes corregir un "Error de sintaxis"?')
+print('Debido a que el error de sintaxis es una violación de las reglas gramaticales, python te indica dónde fue detectado el fallo y puede solucionarse encontrando el error y corrigiendo la gramática')
