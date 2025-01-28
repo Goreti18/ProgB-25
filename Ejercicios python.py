85f1db5b7b8c0c1017cb3d92ca1400bec7279308
Goreti18 <goreti.pizarrop@uanl.edu.mx>
2025-01-27 18:37:26 -0600

    Add files via upload

---
+++ Ejercicios python.py
@@ -0,0 +1,45 @@
+Ejercicio 1
+¿Cuál es la función de la memoria secundaria en una computadora?
+R= Almacenar información durante mucho tiempo, incluso después de ciclos de apagado y encendido
+
+Ejercicio 2
+Un programa son instrucciones almacenadas (órdenes) que se utilizan para la realización de alguna tarea específica.
+
+Ejercicio 3
+¿Cuál es la diferencia entre un compilador y un intérprete?
+R=un intérprete lee el código del programa tal y como fue escrito por el programador, lo analiza e interpreta las instrucciones sobre la marcha, y es capaz de mantener una conversación interactiva, mientras que un compilador necesita el programa completo y luego ejecuta un proceso para traducir el código fuente de alto nivel a código máquina para luego tener un fichero con su ejecución
+
+Ejercicio 4
+¿Cuál de los siguientes contiene "código maquina"?
+R= c) el código fuente de Python
+
+Ejercicio 5
+¿Qué está mal en el siguiente código?
+R= "primt" está mal escrito, faltan los paréntesis en la frase y el mensaje puede ser escrito de una forma más sencilla
+como:
+print("¡Hola, mundo!")
+
+
+Ejercicio 6
+¿En qué lugar del computador queda almacenada una variable, como en este caso "x", después de ejecutar la siguiente línea de Python?
+x=123
+R= Memoria principal
+
+Ejercicio 7
+¿Qué mostrará en pantalla el siguiente programa?
+x = 43
+x = x + 1
+print(x)
+R= 44
+
+Ejercicio 8
+1. Unidad central de procesamiento: el cerebro
+2. Memoria principal: memoria a corto plazo
+3. Memoria secundaria: memoria a largo plazo
+4. Dispositivos de entrada: oídos/capacidad de escuchar e incluso cualquiera de nuestros sentidos
+5. Dispositivos de salida: boca/hablar/expresarnos
+
+
+Ejercicio 9
+¿Cómo puedes corregir un "Error de sintaxis"?
+Debido a que el error de sintaxis es una violación de las reglas gramaticales, python te indica dónde fue detectado el fallo y puede solucionarse encontrando el error y corrigiendo la gramática
